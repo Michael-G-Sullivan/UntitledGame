ec8b9dfe2406951c07cf564ac07ecc503c136277
Michael Sullivan <veritasomnias@gmail.com>
2023-11-23 14:09:47 -0800

    2nd commit test on github

--- UntitledGame/src/main/Game.java
+++ UntitledGame/src/main/Game.java
@@ -3,7 +3,7 @@ package main;
 public class Game {
 
 	public static void main(String[] args) {
-		System.out.println("This is the default start.");
+		System.out.println("This is the default start. This is me making a 2nd change");
 	}
 
 }
